2020612dcb92df94088e040d90026559aeec589a
George Dragomir <george.dragomir.x@gmail.com>
2022-02-22 20:38:01 +0200

    Add function signatures to language syntax
    Extend language syntax with function signatures (as in they
    don't support any sort of content inside the function scope
    ) made up of an identifier and a list of arguments, which
    are pairs of an identifier and a type.

--- src/syntax.rs
+++ src/syntax.rs
@@ -9,12 +9,14 @@ use chumsky::{
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Delimiter {
+    Paren,
     Brace,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Token {
     Struct,
+    Func,
     Ident(String),
 
     Comma,
@@ -31,13 +33,16 @@ impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Token::Struct => write!(f, "struct"),
+            Token::Func => write!(f, "func"),
             Token::Ident(id) => write!(f, "ident({})", id),
             Token::Comma => write!(f, ","),
             Token::Colon => write!(f, ":"),
             Token::Semicolon => write!(f, ";"),
             Token::Lt => write!(f, "<"),
             Token::Gt => write!(f, ">"),
+            Token::Open(Delimiter::Paren) => write!(f, "("),
             Token::Open(Delimiter::Brace) => write!(f, "{{"),
+            Token::Close(Delimiter::Paren) => write!(f, ")"),
             Token::Close(Delimiter::Brace) => write!(f, "}}"),
         }
     }
@@ -49,6 +54,10 @@ pub enum Stmt {
         name: String,
         generics: Option<Vec<String>>,
         fields: Option<Vec<(String, String)>>,
+    },
+    Func {
+        name: String,
+        args: Vec<(String, String)>,
     },
 }
 
@@ -62,11 +71,16 @@ pub fn lexer() -> impl chumsky::Parser<char, Vec<Token>, Error = Simple<char>> {
     ));
 
     let delim = choice((
+        just('(').to(Token::Open(Delimiter::Paren)),
         just('{').to(Token::Open(Delimiter::Brace)),
+        just(')').to(Token::Close(Delimiter::Paren)),
         just('}').to(Token::Close(Delimiter::Brace)),
     ));
 
-    let kw = keyword("struct").to(Token::Struct);
+    let kw = choice((
+        keyword("struct").to(Token::Struct),
+        keyword("func").to(Token::Func),
+    ));
 
     let ident = ident().map(|id| Token::Ident(id));
 
@@ -111,5 +125,21 @@ pub fn parser() -> impl chumsky::Parser<Token, Vec<Stmt>, Error = Simple<Token>>
             fields,
         });
 
-    r#struct.repeated().then_ignore(end())
+    let arg = ident.then_ignore(just(Token::Colon)).then(ident);
+    let args = arg.separated_by(just(Token::Comma)).delimited_by(
+        just(Token::Open(Delimiter::Paren)),
+        just(Token::Close(Delimiter::Paren)),
+    );
+
+    let block = just(Token::Open(Delimiter::Brace))
+        .ignore_then(just(Token::Close(Delimiter::Brace)))
+        .ignored();
+
+    let func = just(Token::Func)
+        .ignore_then(ident)
+        .then(args)
+        .then_ignore(block)
+        .map(|(name, args)| Stmt::Func { name, args });
+
+    r#struct.or(func).repeated().then_ignore(end())
 }
